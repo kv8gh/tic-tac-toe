67fff09cd468920d834245c95a276501d9d299be
kv8gh <vyaskaran1409@gmail.com>
2024-06-23 12:15:07 +0530

    width change

--- app/component/ThreeDCardDemo.tsx
+++ app/component/ThreeDCardDemo.tsx
@@ -18,14 +18,14 @@ export default function ThreeDCardDemo({ src, alt, data }: ThreeDCardDemoProps) 
     <CardContainer>
       <CardBody className={`${
   data === 'X' ? 'bg-red-600' : 'bg-blue-600'
-} relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto md:w-64 h-48 md:h-64 rounded-xl p-6 border`}
+} relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-36 md:w-64 h-40 md:h-64 rounded-xl p-6 border`}
 >
         <CardItem translateZ="100" className="w-full">
           <Image
             src={src}
             height="300"
             width="300"
-            className="h-36 md:h-52 w-full object-fit rounded-xl group-hover/card:shadow-xl"
+            className="h-28 md:h-52 w-full object-fit rounded-xl group-hover/card:shadow-xl"
             alt={alt}
             onClick={()=>{
               router.push(`/game?data=${encodeURIComponent(data)}`);
